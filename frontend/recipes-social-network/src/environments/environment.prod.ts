182b18ea22f0818df60d788b9879857ab397a59c
Iván <ivangomrod@gmail.com>
2021-10-27 12:24:00 +0100

    Update the environment variable with the AWS external URL

--- frontend/recipes-social-network/src/environments/environment.prod.ts
+++ frontend/recipes-social-network/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:4200/api/v0'
+  appName: 'Recipes Social Network',
+  apiHost: 'https://3F077219D5C1687EF1691CF0E201721B.gr7.us-east-2.eks.amazonaws.com:8080/api/v0'
 };
