c070648e565d30412320e9977b6ca0e24eee44f3
Iván <ivangomrod@gmail.com>
2021-10-27 14:16:19 +0100

    Update the URL reverseproxy in frontend api host

--- frontend/recipes-social-network/src/environments/environment.prod.ts
+++ frontend/recipes-social-network/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   appName: 'Recipes Social Network',
-  apiHost: 'https://3F077219D5C1687EF1691CF0E201721B.gr7.us-east-2.eks.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a3825b5213a214e949219168354df278-1567920855.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
