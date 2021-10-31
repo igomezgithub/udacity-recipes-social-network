898c73ef05c360d844f040aef60fa84935900fce
Iván <ivangomrod@gmail.com>
2021-10-31 22:53:41 +0100

    Move the recipe-page html to recipe-list html

--- frontend/recipes-social-network/src/app/features/recipe/recipe-routing.module.ts
+++ frontend/recipes-social-network/src/app/features/recipe/recipe-routing.module.ts
@@ -4,12 +4,13 @@ import { RecipePageComponent } from './pages/recipe-page.component';
 import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './components/recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { AuthGuard } from '../auth/services/auth.guard';
 
 const recipeRoutes: Routes = [
   // { path: '', component: RecipePageComponent,
   //   canActivate: [AuthGuard],
   //   children: [
-  //     { path: 'recipes', component: RecipeListComponent,
+  //      { path: '', component: RecipeListComponent,
   //       children: [
   //         { path: 'list', component: RecipeListComponent },
   //         { path: 'new', component: RecipeItemComponent },
@@ -26,7 +27,7 @@ const recipeRoutes: Routes = [
   //       ]
   //     }
   //   ]
-  // }
+  //}
 ];
 
 @NgModule({
